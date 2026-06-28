b8374c1683dc787f4ea83a495571d53c75c98a52
ayibro <ayibro@gmail.com>
2026-06-28 11:11:03 -0400

    Update SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/YOUR_NEW_SCRIPT_URL/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxd2Z3gQhHAkWNgARb9PXsrDiUoKZtudv0HGWQChaZBV7YGwrBFatTlE62N_37wpzxz/exec",
   ADMIN_PIN: "405",
   POINTS_PER_CORRECT: 3,
 };
